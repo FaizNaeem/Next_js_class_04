6ac7d9095cfe281047cd613e8cc2c95bc434ceb4
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-29 19:17:07 +0500

    world

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -50,10 +50,9 @@ const Navbar = () => {
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    {/* <form class="form-inline my-2 my-lg-0"> */}
+  
   
       <button class="btn btn-outline-success my-2 my-sm-0" onClick={handleOnClickLoginBtn} type="submit">{user ? user.email : "Login"}</button>
-    {/* </form> */}
   </div>
 </nav>
 <div>
